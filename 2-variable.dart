266b4f7e536079221d87e17d3f0ce1ebbf752269
Oumar <foumar404@gmail.com>
2022-02-17 00:13:50 +0100

    add to variable

--- 2-variable.dart
+++ 2-variable.dart
@@ -1,8 +1,39 @@
 void main() {
-  var int = 24;
-  var String = "Omar";
-  var double = 12.4;
-  print(int);
-  print(String);
-  print(double);
+  // var int = 24;
+  // var String = "Omar";
+  // var double = 12.4;
+  // print(int);
+  // print(String);
+  // print(double);
+
+  // const myAge = 42;
+  // var dog = 101;
+  // dog++;
+  // print(myAge);
+  // print(dog);
+
+  // var titre = 43;
+  // print(titre);
+  // titre = 50;
+  // print(titre);
+
+  // const x = 23;
+  // const y = 34;
+
+  // const answer1 = (x * 100) + y;
+  // print(answer1);
+
+  // const answer2 = (x * 100) + (y * 100);
+  // print(answer2);
+
+  // const answer3 = (x * 100) + (y / 100);
+  // print(answer3);
+
+  const rating1 = 12;
+  const rating2 = 24;
+  const rating3 = 25;
+  const rating4 = 27;
+  const rating5 = 28;
+  const averRetaing = (rating1 + rating2 + rating3 + rating4 + rating5) / 3;
+  print(averRetaing);
 }
