a1ab7d36dee9bd17f8e4753946f6f96df2a58f7a
Oumar <foumar404@gmail.com>
2022-02-17 12:11:57 +0100

    adding to variable

---
+++ 3-String-and-string-interpolation.dart
@@ -0,0 +1,9 @@
+void main() {
+  const number1 = 21;
+  const number2 = 42;
+  const number3 = 84;
+  const number4 = 74;
+
+  const mySommeNumber = ((number1 / number2) + (number3 / number4));
+  print(mySommeNumber);
+}
