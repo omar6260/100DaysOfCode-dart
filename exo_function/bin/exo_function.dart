0094bbcf6f2923eeb77bbb438904675bff2d8fc9
Oumar <foumar404@gmail.com>
2022-03-16 13:23:14 +0100

    add to function of dart

---
+++ exo_function/bin/exo_function.dart
@@ -0,0 +1,17 @@
+void main() {
+  somNumber();
+}
+
+void somNumber() {
+  int myNumber = 771508362;
+
+  if (myNumber == "7734398234") {
+    print("numero invalid");
+  } else if (myNumber == "7778274482") {
+    print("numero invalid");
+  } else if (myNumber == "7737283647") {
+    print("numero invalid");
+  } else {
+    print("votre numero est: $myNumber");
+  }
+}
