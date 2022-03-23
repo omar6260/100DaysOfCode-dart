97c2696709bf8752b213374736d396b93f23435d
Oumar <foumar404@gmail.com>
2022-03-23 13:47:29 +0100

    adding my class

--- exo_function/bin/exo_function.dart
+++ exo_function/bin/exo_function.dart
@@ -1,17 +1,44 @@
+// void main() {
+//   somNumber();
+// }
+
+// void somNumber() {
+//   int myNumber = 771508362;
+
+//   if (myNumber == "7734398234") {
+//     print("numero invalid");
+//   } else if (myNumber == "7778274482") {
+//     print("numero invalid");
+//   } else if (myNumber == "7737283647") {
+//     print("numero invalid");
+//   } else {
+//     print("votre numero est: $myNumber");
+//   }
+// }
+// void main() {
+//   printName();
+// }
+
+// void printName() {
+//   String name = "Oumar";
+//   if (name == "doudou" ) {
+//     print("Name is false");
+//   } else {
+//     print("name is true");
+//   }
+// }
+
+// void main() {
+//   const input = 12;
+//   final output = compliment(input);
+//   print(output);
+// }
+
+// String compliment(int number) {
+//   return '$number is a very nice number';
+// }
 void main() {
-  somNumber();
-}
-
-void somNumber() {
-  int myNumber = 771508362;
-
-  if (myNumber == "7734398234") {
-    print("numero invalid");
-  } else if (myNumber == "7778274482") {
-    print("numero invalid");
-  } else if (myNumber == "7737283647") {
-    print("numero invalid");
-  } else {
-    print("votre numero est: $myNumber");
-  }
+  const list = [5, 4, 3, 2, 1];
+  final value = list.map((number) => number * 5);
+  print('new values are: ${value}');
 }
---
+++ my_class/bin/my_class.dart
@@ -0,0 +1,59 @@
+// void main() {
+//   String name = "Oumar";
+//   int age = 23;
+
+//   String speak(String words) {
+//     print("what i am trying to say is, " + words);
+//     return ("what i am trying to say is " + words);
+//   }
+
+//   String sayAge(int myAge) {
+//     print("My age is, " + sayAge(myAge));
+//     return ("My age is, " + sayAge(myAge));
+//   }
+// }
+
+// void main() {
+//   print(Person());
+// }
+
+// class Person {
+//   String name = "Oumar";
+//   int age = 23;
+
+//   Person() {
+//     this.name = this.name.toUpperCase();
+//     this.age = this.age + 3;
+//   }
+//   // Person(String nameNew, int ageNew) {
+//   //   this.name = nameNew;
+//   //   this.age = ageNew;
+//   // }
+//   // Person(this.name, this.age);
+
+// }
+
+// void main() {
+//   final user = User();
+//   user.name = 'Ray';
+//   user.id = 42;
+//   print(user);
+// }
+
+// class User {
+//   int id = 0;
+//   String name = '';
+//   @override
+//   String toString() {
+//     return 'User(id: $id, name: $name)';
+//   }
+// }
+
+void main() {
+  getInfo("Oumar", 771508362);
+}
+
+void getInfo(String name, int num) {
+  print(name.toUpperCase());
+  print(num.isEven);
+}
