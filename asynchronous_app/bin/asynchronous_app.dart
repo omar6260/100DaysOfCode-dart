0c257a6a8e9ff65de19309b0f7443ea934b71ce4
Oumar Fall <foumar404@gmail.com>
2022-04-19 16:58:10 +0100

    ajout du exo dart

--- asynchronous_app/bin/asynchronous_app.dart
+++ asynchronous_app/bin/asynchronous_app.dart
@@ -121,23 +121,25 @@ import 'dart:io' show exit;
 //   }
 // }
 
-Future<void> printOrderMessage() async {
-  try {
-    print('Awaiting user order...');
-    var order = await fetchUserOrder();
-    print(order);
-  } catch (err) {
-    print('Caught error: $err');
-  }
-}
+// Future<void> printOrderMessage() async {
+//   try {
+//     print('Awaiting user order...');
+//     var order = await fetchUserOrder();
+//     print(order);
+//   } catch (err) {
+//     print('Caught error: $err');
+//   }
+// }
 
-Future<String> fetchUserOrder() {
-  // Imagine that this function is more complex.
-  var str = Future.delayed(
-      const Duration(seconds: 4), () => throw 'Cannot locate user order');
-  return str;
-}
+// Future<String> fetchUserOrder() {
+//   // Imagine that this function is more complex.
+//   var str = Future.delayed(
+//       const Duration(seconds: 4), () => throw 'Cannot locate user order');
+//   return str;
+// }
 
-void main() async {
-  await printOrderMessage();
-}
+// void main() async {
+//   await printOrderMessage();
+// }
+
+
