aefaad623652a2382de01cc7e78da4fc905cd229
Oumar Fall <foumar404@gmail.com>
2022-04-30 13:57:48 +0100

    revision en dart

---
+++ revision_app/bin/revision_app.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age = 23;
+  String name = "omar";
+  double taile = 1.14;
+  bool adult = true;
+
+  print(age);
+  print(name);
+  print(taile);
+  print(adult);
+  print("je me nomme $name, j'ai $age et ma taile et de $taile");
+}
