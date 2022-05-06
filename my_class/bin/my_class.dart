f2e7dfa73132e78ee4b883c4b4a6165184d6e9ba
Oumar Fall <foumar404@gmail.com>
2022-05-06 14:28:14 +0100

    revision dart

--- my_class/bin/my_class.dart
+++ my_class/bin/my_class.dart
@@ -58,13 +58,67 @@
 //   print(num.isEven);
 // }
 
-class Person {
-  var name = "Oumar";
-  String sayHello(String name) {
-    return "Hello $name";
+// declaration
+// class Person {
+//   var name = "Oumar";
+//   String sayHello(String name) {
+//     return "Hello $name";
+//   }
+// }
+
+// // inhertance
+// class Employee extends Person {}
+
+// // abstract class
+// abstract class Certification {
+//   String getType() {
+//     return 'mobile dev';
+//   }
+// }
+
+// void main() {
+//   const input = 12;
+//   final output = compliment(input);
+//   print(output);
+//   var result = sum(100, 134);
+//   print('result $result');
+//   greeting('Tom');
+// }
+
+// //
+// String compliment(int number) {
+//   return '$number is a very nice number';
+// }
+
+// int sum(int a, int b) {
+//   return a + b;
+// }
+
+// void greeting(String name) {
+//   var s = 'Hello $name';
+//   print(s);
+// }
+String concat(String s1, String s2, [String? s3]) {
+  if (s3 != null) {
+    return s1 + s2 + s3;
   }
+  return s1 + s2;
+}
+
+double sum(double v1, double v2, [double? v3, double? v4]) {
+  return v1 + v2 + (v3 ?? 0) + (v4 ?? 0);
 }
 
 void main() {
-  Person();
+  String result1 = concat('One', 'Two');
+  print('result $result1');
+  String result2 = concat('One', 'Two', 'three');
+  print('result: $result2');
+
+  double val1 = sum(1, 2, 3, 4);
+  print('value1: $val1');
+  double val2 = sum(1, 2, 3);
+  print('value: $val2');
+  double val3 = sum(1, 2);
+  print('value: $val3');
 }
